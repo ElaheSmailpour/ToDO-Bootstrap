c96d6daabd885207ca48df02a2b29ea5d6baeca1
Elahesmailpour <eli_es87@yahoo.com>
2021-06-17 11:51:39 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipesComponent } from './recipes/recipes.component';
+
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+const routes: Routes = [
+
+  {path:"recipe",component:RecipesComponent},
+  {path:"shopping",component:ShoppingEditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
